12a2ab7cad1d39331ef6025f3b83a8f39af1d08e
John Wallin <jfwallin@gmail.com>
2018-03-02 15:16:22 -0600

    Added starwheel.py and skymap.py Starwheel is a refactoring of the astrolabe program. Skymap is a new projection to make a monthly skymap. I also updated the projection routines and graphics routines to do the monthly maps.  This is still a work in progress.

---
+++ skymap.py
@@ -0,0 +1,118 @@
+
+from starGraphics import starGraphics as sg
+from project import project as project
+from astronomy import astronomy as astronomy
+from starData import starData as starData
+
+class astrolabe:
+
+    def __init__(self):
+        self.s = starData()
+        self.p = project()
+        self.a = astronomy()
+       
+        pltCircle = 1.0 
+        pltSize = pltSize = 1.1 * pltCircle
+        pltLimit = pltCircle 
+
+        pltStellarMagnitudeLimit = 4.8
+
+        self.pltConstellationLines = 1
+        self.pltNebula = 0
+        self.pltConstellationBoundaries = 1
+        self.pltConstellationLabels = 1
+        self.pltEclipticLine = 1
+        self.pltDecLines = 1
+        self.pltRALines = 1
+        self.pltDateCircle = 0
+
+        # plot the Rete
+        self.g = sg(-pltSize, pltSize, -pltSize, pltSize, pltLimit)
+        self.g.setStarListIndex(pltStellarMagnitudeLimit)
+
+        decMax = -23.5
+        lat = 30 
+        decMax = -90 + lat 
+        rMax = pltLimit
+        self.plotStarMap(decMax, rMax)
+        self.g.endPlot("rete")
+
+
+
+        
+
+    def plotStarMap(self, decMax, rMax):
+
+        self.p.setProjection(3, 0, 90, decMax, rMax )
+        lst = 9.
+        lat = 36.
+        self.p.setTime(lst, lat)
+        pStars = self.p.projectStars( self.s.stars, 5, 6)
+        self.g.plotStars(pStars)
+        #self.g.plotStarLabels(pStars, 2)
+        #self.g.plotStarLabels(pStars, 1)
+
+        aStars = self.p.projectStars( self.s.astars, 5, 6)
+        self.g.plotAstrolabeStarLabels(aStars, 10)
+        
+        # this is needed for the boundaries and the labels
+        pConstellationBoundaries= self.p.projectConstellationBoundaries( self.s.constellationBoundaries)
+
+
+        if self.pltConstellationLines:
+            pConstellationLines = self.p.projectConstellationLines( self.s.constellationLines)
+            self.g.plotConstellationLines(pConstellationLines)
+        
+        if self.pltNebula:
+            pNebula = self.p.projectNebula( self.s.nebula)
+            self.g.plotNebula(pNebula, 5)
+        
+        if self.pltConstellationBoundaries:
+            self.g.plotConstellationBoundaries(pConstellationBoundaries)
+
+        if self.pltConstellationLabels:
+            self.g.plotConstellationLabels2(pConstellationBoundaries)
+            #self.g.plotConstellationLabels(pConstellationBoundaries)
+        
+        if self.pltEclipticLine:
+            eclipLine = self.a.createEclipticPath()
+            pEclipLine = self.p.projectSimpleLine(eclipLine)
+            self.g.plotSimpleLine(pEclipLine)
+
+        if self.pltDecLines:
+            decList = [90, 80, 70, 60, 50, 40, 30, 20, 10, 0, -10, -20]
+            self.g.plotDecCircles(decList)
+
+        if self.pltRALines:
+            #raHours = [0]
+            raHours = range(0,24)
+            self.g.plotRaLines( raHours, decMax)
+        
+        if self.pltDateCircle:
+            self.g.dateCircle()
+
+
+    def plotHorizon(self, hrotate):
+
+        npts = 60
+        altLine = self.a.createAltPath( 0.0, npts, hrotate)
+        pAltLine = self.p.projectSimpleLine(altLine)
+        self.g.plotSimpleLine(pAltLine)
+
+
+ 
+
+
+if __name__ == "__main__":
+
+    A = astrolabe()
+
+
+#    decLine = p.createDecLine(0)
+#    g.plotSimpleLine(decLine)
+
+#    decLine = p.createDecLine(45)
+#    g.plotSimpleLine(decLine)
+
+
+
